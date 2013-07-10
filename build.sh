253e56ff7814e9e961c53e349cd83d01c749b7d5
Henri Wahl <h.wahl@ifw-dresden.de>
2013-07-10 13:47:08 +0200

    added debian postinst for non-root user

--- build.sh
+++ build.sh
@@ -29,30 +29,33 @@ elif [ -f /etc/redhat-release ]
 	then
 		echo "Building .rpm package"
 
-		# setup.py checks if this file exists
-		touch /tmp/DHCPY6D_BUILDING_RPM
+        TOPDIR=$HOME/dhcpy6d.$$
+        SPEC=installer/redhat/dhcpy6d.spec
 
 		# create source folder for rpmbuild
-		mkdir -p ~/rpmbuild/SOURCES
+		mkdir -p $TOPDIR/SOURCES
 	
-		# build spec only because it needs to be modified
-		python setup.py bdist_rpm --dist-dir . --spec-only
-
-		# modify spec file to keep custom configuration when updating
-		echo "%config(noreplace) /etc/dhcpy6d.conf" >> ./dhcpy6d.spec
-		echo "%config(noreplace) /var/lib/dhcpy6d/volatile.sqlite" >> ./dhcpy6d.spec
-        echo "%config(noreplace) /var/log/dhcpy6d.log" >> ./dhcpy6d.spec
+        # init needed in TOPDIR/SOURCES
+        cp -pf installer/redhat/init.d/dhcpy6d $TOPDIR/SOURCES
 
 		# use setup.py sdist build output to get package name
-		FILE=`python setup.py sdist --dist-dir ~/rpmbuild/SOURCES | grep "creating dhcpy6d-" | head -n1 | cut -d" " -f2`
+		FILE=`python setup.py sdist --dist-dir $TOPDIR/SOURCES | grep "creating dhcpy6d-" | head -n1 | cut -d" " -f2`
 		echo Source file: $FILE.tar.gz
 
+        # version
+        VERSION=`echo $FILE | cut -d"-" -f 2`
+
+        # replace version in the spec file
+        sed -i "s|Version:.*|Version: $VERSION|" $SPEC
+
 		# finally build binary rpm
-		rpmbuild -bb dhcpy6d.spec
+		rpmbuild -bb --define "_topdir $TOPDIR" $SPEC
 
 		# get rpm file
-		cp ~/rpmbuild/RPMS/noarch/$FILE-1.noarch.rpm .
+		cp -f `find $TOPDIR/RPMS -name "$FILE-1.*noarch.rpm"` .
 
+        # clean
+        rm -rf $TOPDIR
 else
 	echo "Package creation is only supported on Debian and RedHat derivatives."
 fi
