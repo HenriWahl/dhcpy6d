c0abc13e4dca09b91fd15e9116ba257b49081fd4
Axel Beckert <abe@deuxchevaux.org>
2014-03-21 11:14:35 +0100

    Update build.sh to work with new debian directory location

--- build.sh
+++ build.sh
@@ -9,20 +9,7 @@ if [ -f /etc/debian_version ]
 	then
 		echo "Building .deb package"
 		
-		cd installer
-		ln -s ../etc
-		ln -s ../dhcpy6d
-		ln -s ../dhcpy6
-		ln -s ../doc
-		ln -s ../var
-		ln -s ../setup.py
-
-		cp etc/default/dhcpy6d debian/dhcpy6d.default
-		cp etc/logrotate.d/dhcpy6d debian/dhcpy6d.logrotate
-		cp etc/init.d/dhcpy6d debian/dhcpy6d.init
-
-		chmod 755 debian/rules
-		dh clean --with python2
+                debuild clean
 		debuild binary-indep
 
 elif [ -f /etc/redhat-release ]
