6e4051c315be64bed67edbfc66c6743a0733b05a
Felix Jacobi <Felix@jacobi-hamburg.net>
2018-10-25 09:44:14 +0200

    Fixed comma vs. dot

--- dhcpy6/config.py
+++ dhcpy6/config.py
@@ -846,7 +846,7 @@ class Config(object):
                 # test validity of category
                 if not self.ADDRESSES[a].CATEGORY.strip() in ['eui64', 'fixed', 'range', 'random', 'mac', 'id', 'dns']:
                     error_exit(
-                        "%s Category '%s' is invalid. Category must be one of 'eui64'. 'fixed', 'range', 'random', 'mac', 'id' and 'dns'." % (
+                        "%s Category '%s' is invalid. Category must be one of 'eui64', 'fixed', 'range', 'random', 'mac', 'id' and 'dns'." % (
                         msg_prefix, self.ADDRESSES[a].CATEGORY))
 
                 # test validity of pattern - has its own error output
